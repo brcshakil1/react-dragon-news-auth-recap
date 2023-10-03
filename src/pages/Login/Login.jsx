dcea1a1cab5c93eaf01f5f69dbf40657cecaf5c7
brcshakil1 <brcshakil72@gmail.com>
2023-10-03 19:04:17 +0600

    react dragon news recap

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./../Shared/Navbar/Navbar";
+const Login = () => {
+  return (
+    <div>
+      <Navbar />
+      <h2 className="text-xl">This is log in</h2>
+    </div>
+  );
+};
+
+export default Login;
